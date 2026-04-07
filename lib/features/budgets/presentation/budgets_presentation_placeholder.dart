b4518c2fab431ffab07c3e3d4a94100a537992fe
Jimsalm <hypermood15.jimiel@gmail.com>
2026-04-07 23:16:15 +0800

    refactor(budgets-ui): export budgets screen and widget modules

--- lib/features/budgets/presentation/budgets_presentation_placeholder.dart
+++ lib/features/budgets/presentation/budgets_presentation_placeholder.dart
@@ -1,1 +1,4 @@
-export 'screens/budgets_screen.dart';
+export 'screens/budgets_tab_screen.dart';
+export 'widgets/budget_allocation_card.dart';
+export 'widgets/budget_overview_card.dart';
+export 'widgets/empty_budgets_card.dart';

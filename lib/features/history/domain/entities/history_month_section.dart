231d2060ea757298612f65402fc1094d65203899
Jimsalm <hypermood15.jimiel@gmail.com>
2026-04-08 17:40:20 +0800

    feat(history): add HistoryMonthSection domain model

---
+++ lib/features/history/domain/entities/history_month_section.dart
@@ -0,0 +1,15 @@
+import 'package:budget_tracker/features/history/domain/entities/history_item.dart';
+
+class HistoryMonthSection {
+  const HistoryMonthSection({
+    required this.year,
+    required this.month,
+    required this.totalCentavos,
+    required this.items,
+  });
+
+  final int year;
+  final int month;
+  final int totalCentavos;
+  final List<HistoryItem> items;
+}

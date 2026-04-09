6ff304ef6893da0aceefabab776e6e799fa803fe
Jimsalm <hypermood15.jimiel@gmail.com>
2026-04-09 14:11:17 +0800

    feat(settings-usecase): add save spending-threshold-alerts use case

---
+++ lib/features/settings/domain/usecases/save_spending_threshold_alerts_enabled_use_case.dart
@@ -0,0 +1,11 @@
+import 'package:budget_tracker/features/settings/domain/repositories/settings_repository.dart';
+
+class SaveSpendingThresholdAlertsEnabledUseCase {
+  const SaveSpendingThresholdAlertsEnabledUseCase(this._repository);
+
+  final SettingsRepository _repository;
+
+  Future<void> call(bool enabled) {
+    return _repository.saveSpendingThresholdAlertsEnabled(enabled);
+  }
+}

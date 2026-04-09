46a76d05310466a3b53b0ccc36bcca141e5fcd42
Jimsalm <hypermood15.jimiel@gmail.com>
2026-04-09 13:54:18 +0800

    feat(app): initialize app-level settings providers at startup

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,12 +1,20 @@
+import 'package:budget_tracker/app/providers/providers.dart';
 import 'package:budget_tracker/app/router/app_router.dart';
 import 'package:budget_tracker/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class BudgetTrackerApp extends StatelessWidget {
+class BudgetTrackerApp extends ConsumerWidget {
   const BudgetTrackerApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(appCurrencyCodeProvider);
+    ref.watch(appHardBudgetModeProvider);
+    ref.watch(appSpendingThresholdAlertsProvider);
+    ref.watch(appPrimaryWarningLevelProvider);
+    ref.watch(appOcrScannerEnabledProvider);
+
     return MaterialApp(
       title: 'Shopping Budget Tracker',
       debugShowCheckedModeBanner: false,

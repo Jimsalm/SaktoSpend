8aae3ec8040c8fb326ba627cc5263f9ca9cdb357
Jimsalm <hypermood15.jimiel@gmail.com>
2026-04-09 14:10:48 +0800

    feat(settings-usecase): add save currency code use case

---
+++ lib/features/settings/domain/usecases/save_currency_code_use_case.dart
@@ -0,0 +1,11 @@
+import 'package:budget_tracker/features/settings/domain/repositories/settings_repository.dart';
+
+class SaveCurrencyCodeUseCase {
+  const SaveCurrencyCodeUseCase(this._repository);
+
+  final SettingsRepository _repository;
+
+  Future<void> call(String currencyCode) {
+    return _repository.saveCurrencyCode(currencyCode);
+  }
+}

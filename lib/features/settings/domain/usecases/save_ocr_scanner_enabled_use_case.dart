00aaeec14451b2191227f29b4eb97bde03405637
Jimsalm <hypermood15.jimiel@gmail.com>
2026-04-09 14:10:59 +0800

    feat(settings-usecase): add save OCR scanner enabled use case

---
+++ lib/features/settings/domain/usecases/save_ocr_scanner_enabled_use_case.dart
@@ -0,0 +1,11 @@
+import 'package:budget_tracker/features/settings/domain/repositories/settings_repository.dart';
+
+class SaveOcrScannerEnabledUseCase {
+  const SaveOcrScannerEnabledUseCase(this._repository);
+
+  final SettingsRepository _repository;
+
+  Future<void> call(bool enabled) {
+    return _repository.saveOcrScannerEnabled(enabled);
+  }
+}

9eaa052bb0b1b7a738be97501a514b8d5152c246
Jimsalm <hypermood15.jimiel@gmail.com>
2026-04-09 14:10:11 +0800

    feat(settings-usecase): add get hard budget mode use case

---
+++ lib/features/settings/domain/usecases/get_hard_budget_mode_use_case.dart
@@ -0,0 +1,11 @@
+import 'package:budget_tracker/features/settings/domain/repositories/settings_repository.dart';
+
+class GetHardBudgetModeUseCase {
+  const GetHardBudgetModeUseCase(this._repository);
+
+  final SettingsRepository _repository;
+
+  Future<bool> call() {
+    return _repository.getHardBudgetMode();
+  }
+}
